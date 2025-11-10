b5237eafd20952fe8c0d5f68952c87d04068cf5f
ehtesham999 <ehteshamhassan1@gmail.com>
2025-11-10 22:30:56 +0500

    password hash and requirements

--- database.py
+++ database.py
@@ -9,3 +9,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
